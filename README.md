256ace8581c93e8d33d62907fe189589f84a4863
Dave Woodward <davidwoodwrd@hotmail.com>
2018-02-12 16:37:59 -0600

    Update to handle empty proxy ports and update documentation.

--- README.md
+++ README.md
@@ -2,23 +2,30 @@
 Defines a WSO2 API Manager platform docker image that enables the consumer to override the most commonly overridden aspects of the default platform configuration.
 
 ## How to Use
-1. Run the cooresponding database container available on [Docker Hub](https://hub.docker.com/r/salte/mysql-wso2am/).
+1. Run the corresponding database container available on [Docker Hub](https://hub.docker.com/r/salte/mysql-wso2am/).
 2. Run the image counterpart of this manifest available on [Docker Hub](https://hub.docker.com/r/salte/wso2am/) via the following command:
 ```
-$ docker run --name=wso2am --restart=always --detach \
-                --volume=<host volume>:<container volume> \
-                --publish=9443:9443 \
-                --publish=9763:9763 \
-                --publish=8243:8243 \
-                --publish=8280:8280 \
-                -e EXTERNAL_HOSTNAME=<DNS Hostname> \
-                -e DATABASE_HOSTNAME=<DNS Hostname> \
-                -e DATABASE_PORT=<port number> \
-                -e DATABASE_USER=<username> \
-                -e DATABASE_PASSWORD=<password> \
-                -e PUBLIC_CERTIFICATE=<public certificate file> \
-                -e PRIVATE_KEY=<private key file> \
-                -e PRIVATE_KEY_PASSWORD=<password> \
-                -e ADMIN_PASSWORD=<password> wso2am:<tag>
+$ docker run --name wso2am --restart=always --detach \
+             --publish=9443:9443 \
+             --publish=9763:9763 \
+             --publish=8280:8280 \
+             --publish=8243:8243 \
+             --volume=<host volume>:<container volume> \
+             -e EXTERNAL_HOSTNAME=<DNS Hostname> \
+             -e DATABASE_HOSTNAME=<DNS Hostname> \
+             -e DATABASE_PORT=<port number> \
+             -e DATABASE_USER=<username> \
+             -e DATABASE_PASSWORD=<password> \
+             -e PUBLIC_CERTIFICATE=<path and filename> \
+             -e PRIVATE_KEY=<path and filename> \
+             -e PRIVATE_KEY_PASSWORD=<password> \
+             -e ADMIN_PASSWORD=<password> \
+             -e DELAY=0 \
+             -e OFFSET=0 \
+             -e SSL_PROXY_PORT=443 \
+             -e PROXY_PORT=80 \
+             -e JWT_EXPIRY=5 \
+             -e CA_CERTIFICATE_BUNDLE=<path and filename> \
+             wso2am:<tag>
 ```
-In the example above, "host volume" is the location on disk where the consumer is placing both the "public certificate file" and the "private key file" that will be used to secure https communications.  In addition, "container volume" is where the "host volume" will be mounted inside of the container, thus making the aforementioned "public certificate file" and "private key file" available to the container's initialization process.  The value provided for "public certificate file" and "private key file" should incorporate this path within their respective values.
+In the example above, "host volume" is the location on disk where the "public certificate file" and the "private key file" can be found.  "container volume" is where the "host volume" will be mounted inside of the container; making the aforementioned "public certificate file" and "private key file" available to the container's initialization process.  The value provided for "public certificate file" and "private key file" should incorporate this path within their respective values.  These files will be used to secure https communications.
--- wso2am-entrypoint.sh
+++ wso2am-entrypoint.sh
@@ -3,27 +3,52 @@ set -eo pipefail
 
 if [ ! -f "/opt/wso2am/repository/conf/initialized" ]; then
   cd /opt/wso2am/repository/resources/security
+
+  # Prepare Keystore
   openssl pkcs12 -export -in $PUBLIC_CERTIFICATE -inkey $PRIVATE_KEY -out wso2carbon.p12 -name wso2carbon -passout pass:$PRIVATE_KEY_PASSWORD
   keytool -delete -alias wso2carbon -keystore wso2carbon.jks -storepass wso2carbon
   keytool -importkeystore -noprompt -deststorepass wso2carbon -destkeypass $PRIVATE_KEY_PASSWORD -destkeystore wso2carbon.jks -srckeystore wso2carbon.p12 -srcstoretype PKCS12 -srcstorepass $PRIVATE_KEY_PASSWORD -alias wso2carbon
   keytool -storepasswd -new $PRIVATE_KEY_PASSWORD -keystore wso2carbon.jks -storepass wso2carbon
+
+  # Prepare Client Truststore
   keytool -delete -alias wso2carbon -keystore client-truststore.jks -storepass wso2carbon
   keytool -importcert -noprompt -alias wso2carbon -keystore client-truststore.jks -storepass wso2carbon -file $PUBLIC_CERTIFICATE
   keytool -storepasswd -new $PRIVATE_KEY_PASSWORD -keystore client-truststore.jks -storepass wso2carbon
   keytool -importcert -noprompt -keystore client-truststore.jks -storepass $PRIVATE_KEY_PASSWORD -file $CA_CERTIFICATE_BUNDLE
   rm wso2carbon.p12
+  
+  # Update Configuration Files
   saxonb-xslt -s:/opt/wso2am/repository/conf/axis2/axis2.xml -xsl:/tmp/axis2.xsl -o:/opt/wso2am/repository/conf/axis2/axis2.xml password=$PRIVATE_KEY_PASSWORD keystore=/opt/wso2am/repository/resources/security/wso2carbon.jks
   saxonb-xslt -s:/opt/wso2am/repository/conf/carbon.xml -xsl:/tmp/carbon.xsl -o:/opt/wso2am/repository/conf/carbon.xml hostname=$EXTERNAL_HOSTNAME password=$PRIVATE_KEY_PASSWORD keystore=/opt/wso2am/repository/resources/security/wso2carbon.jks alias=wso2carbon offset=$OFFSET
-  saxonb-xslt -s:/opt/wso2am/repository/conf/api-manager.xml -xsl:/tmp/api-manager.xsl -o:/opt/wso2am/repository/conf/api-manager.xml hostname=$EXTERNAL_HOSTNAME password=$PRIVATE_KEY_PASSWORD jwtexpiry=$JWT_EXPIRY thriftserver=localhost proxyport=$PROXY_PORT sslproxyport=$SSL_PROXY_PORT topicconnectionfactory="amqp://admin!wso2.com!carbon.super:$ADMIN_PASSWORD@clientid/carbon?brokerlist='tcp://localhost:5682'"
   saxonb-xslt -s:/opt/wso2am/repository/conf/identity/identity.xml -xsl:/tmp/identity.xsl -o:/opt/wso2am/repository/conf/identity/identity.xml hostname=$EXTERNAL_HOSTNAME offset=$OFFSET
   saxonb-xslt -s:/opt/wso2am/repository/conf/user-mgt.xml -xsl:/tmp/user-mgt.xsl -o:/opt/wso2am/repository/conf/user-mgt.xml userds=jdbc/WSO2UM_DB password=$ADMIN_PASSWORD
   saxonb-xslt -s:/opt/wso2am/repository/conf/registry.xml -xsl:/tmp/registry.xsl -o:/opt/wso2am/repository/conf/registry.xml sqlhost=$DATABASE_HOSTNAME sqlport=$DATABASE_PORT regds=jdbc/WSO2REG_DB regdb=regdb dbuser=$DATABASE_USER
   saxonb-xslt -s:/opt/wso2am/repository/conf/datasources/master-datasources.xml -xsl:/tmp/master-datasources.xsl -o:/opt/wso2am/repository/conf/datasources/master-datasources.xml sqlhost=$DATABASE_HOSTNAME sqlport=$DATABASE_PORT apidb=apimgtdb userdb=userdb userds=jdbc/WSO2UM_DB regdb=regdb regds=jdbc/WSO2REG_DB dbuser=$DATABASE_USER dbpassword=$DATABASE_PASSWORD
-  saxonb-xslt -s:/opt/wso2am/repository/conf/tomcat/catalina-server.xml -xsl:/tmp/catalina-server.xsl -o:/opt/wso2am/repository/conf/tomcat/catalina-server.xml password=$PRIVATE_KEY_PASSWORD sslproxyport=$SSL_PROXY_PORT
   saxonb-xslt -s:/opt/wso2am/repository/conf/data-bridge/data-bridge-config.xml -xsl:/tmp/data-bridge-config.xsl -o:/opt/wso2am/repository/conf/data-bridge/data-bridge-config.xml password=$PRIVATE_KEY_PASSWORD
+
+  if [ -z "$PROXY_PORT" ] && ! [[ $PROXY_PORT =~ ^[0-9]+$ ]]; then
+    echo "When specified, PROXY_PORT must be numeric."
+    exit 1
+  elif [ -z "$SSL_PROXY_PORT" ] && ! [[ $SSL_PROXY_PORT =~ ^[0-9]+$ ]]; then
+    echo "When specified, SSL_PROXY_PORT must be numeric."
+    exit 1
+  elif [ -z "$PROXY_PORT" ] && [ -z "$SSL_PROXY_PORT" ]; then
+    saxonb-xslt -s:/opt/wso2am/repository/conf/api-manager.xml -xsl:/tmp/api-manager.xsl -o:/opt/wso2am/repository/conf/api-manager.xml hostname=$EXTERNAL_HOSTNAME password=$PRIVATE_KEY_PASSWORD jwtexpiry=$JWT_EXPIRY thriftserver=localhost proxyport=$PROXY_PORT sslproxyport=$SSL_PROXY_PORT topicconnectionfactory="amqp://admin!wso2.com!carbon.super:$ADMIN_PASSWORD@clientid/carbon?brokerlist='tcp://localhost:5682'"
+    saxonb-xslt -s:/opt/wso2am/repository/conf/tomcat/catalina-server.xml -xsl:/tmp/catalina-server.xsl -o:/opt/wso2am/repository/conf/tomcat/catalina-server.xml password=$PRIVATE_KEY_PASSWORD sslproxyport=$SSL_PROXY_PORT
+  else
+    saxonb-xslt -s:/opt/wso2am/repository/conf/api-manager.xml -xsl:/tmp/api-manager.xsl -o:/opt/wso2am/repository/conf/api-manager.xml hostname=$EXTERNAL_HOSTNAME password=$PRIVATE_KEY_PASSWORD jwtexpiry=$JWT_EXPIRY thriftserver=localhost topicconnectionfactory="amqp://admin!wso2.com!carbon.super:$ADMIN_PASSWORD@clientid/carbon?brokerlist='tcp://localhost:5682'"
+    saxonb-xslt -s:/opt/wso2am/repository/conf/tomcat/catalina-server.xml -xsl:/tmp/catalina-server.xsl -o:/opt/wso2am/repository/conf/tomcat/catalina-server.xml password=$PRIVATE_KEY_PASSWORD
+  fi
+
+  # Clean-up Configuration File Transformation Scripts
   rm /tmp/*.xsl
+
+  # Indicate Container Initialization Complete
   touch /opt/wso2am/repository/conf/initialized
-  if echo $DELAY | egrep -q '^[0-9]+$'; then
+
+  # Sleep to give other containers we're dependent upon a chance to complete
+  # their initialization.
+  if [ -z "$DELAY" ] && [[ $DELAY =~ ^[0-9]+$ ]]; then
     sleep $DELAY
   fi
 fi
